d662e7fad9b5e3b48cd039181d3ee82b8882a175
RihardsJ <rihards.jukna@gmail.com>
2021-02-19 15:54:33 +0000

    finalised the task creat multiples function, corected expected values in unit testing

--- public/main.js
+++ public/main.js
@@ -1,3 +1,25 @@
-const multiples = (input) => {};
+const multiples = (input) => {
+  let number = Number(input);
+  const result = [];
+
+  const highestNumber = {
+    7: 100,
+    8: 200,
+    9: 300,
+  };
+
+  number = highestNumber[number];
+  console.log(number);
+
+  for (let i = input; i < number; i += input) {
+    result.push(i);
+  }
+
+  return result;
+};
+
+console.log(multiples(7));
+console.log(multiples(8));
+console.log(multiples(9));
 
 module.exports = multiples;
